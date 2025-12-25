f7c638f414ad0d1286bd8b56e4a99d3794b58293
zafarshodmonov <zafarshodmonov1996@gmail.com>
2025-12-25 23:23:45 +0500

    modified: pages/tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -11,3 +11,19 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_url_name(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_homepage_contains_correct_html(self):  # new
+        response = self.client.get("/")
+        self.assertContains(response, "home page")
+
+    def test_homepage_does_not_contain_incorrect_html(self):  # new
+        response = self.client.get("/")
+        self.assertNotContains(
+            response, "Hi there! I should not be on the page."
+        )
+
+
